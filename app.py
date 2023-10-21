b6eb6fd5c67f35b3c2637ab161773f4aa8f2f7ff
escowin <edwin@escowinart.com>
2023-10-20 22:20:52 -0500

    wrote pseudocode sequence & programmed conditional statements

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
+# SEQUENCE
+# 1. Import relevant libraries
+# 2. Greet user with init message
+# 3. Prompt user to enter weight & unit
+# 4. Check if the input is valid
+# 5. Handle invalid input
+# 6. Perform the conversion
+# 7. Display the converted value
+
 import datetime
 
+# Init message variables
 date = datetime.date.today()
 app = "unit-converter"
 print(f"""
@@ -8,7 +18,15 @@ print(f"""
       http://github.com/escowin/{app}
       """)
 
+# Terminal prompt variables
 weight = input("weight: ")
 unit = input("(g) or (oz): ")
 
-print(weight + " " + unit)
+# Handles input data through conditional statements
+if unit.lower() == "g":
+    # Converts var from string to float type to allow calculation
+    converted = float(weight) / 28.35 
+    print(f"{weight} {unit} = {converted} oz")
+else:
+    converted = float(weight) * 28.35
+    print(f"{weight} {unit} = {converted} g")
