4867fbd932a682050ae47f522d03332920638ea2
escowin <edwin@escowinart.com>
2023-10-21 20:16:42 -0500

    moved menu options into dictionary

--- app.py
+++ app.py
@@ -12,64 +12,87 @@
 
 import datetime
 
+# global scope variables
 app = "unit-converter"
 calculate = {
     "measurement": ("(l)ength", "(m)ass", "(v)olume"),
     "length": ("(cm)", "(in)"),
     "mass": ("(g)", "(oz)"),
-    "volume": ("(l)itre", "(g)allon")
+    "volume": ("(l)itre", "(g)allon"),
+    "menu": ("(1) calculate new amount", "(2) switch units", "(3) switch measurement", "(0) exit")
 }
 
+
+def generate_prompt(option):
+    # Dynamically generates user prompt
+    prompt = f"\n: : {option.upper()} SELECTION : : \n"
+    for choice in calculate[option]:
+        prompt += f"\n{choice}"
+    prompt += "\n\nenter selection: "
+    return prompt
+
+
 def init():
+    # initial greeting
     date = datetime.date.today()
     print(f"""
     {app} 0.0.2
     \u00A9 {date.year} Edwin M. Escobar
-    http: //github.com/escowin/{app}\n""")
-    select_measurement_type()
+    http: //github.com/escowin/{app}""")
+    select_measurement()
 
-def generate_prompt(option):
-    prompt = f": : {option.upper()} SELECTION : :\n\n"
-    print(calculate[option])
-    for choice in calculate[option]:
-        prompt += f"{choice}\n"
-    prompt += "\nenter: "
-    return prompt
 
-def select_measurement_type():
+def select_measurement():
+    # Displays a prompt containing measurement options
     prompt = generate_prompt("measurement")
-    measurement_type = input(prompt)
-    convert_unit(measurement_type.upper())
+    selected_measurement = input(prompt)
+    select_unit(selected_measurement.lower())
 
 
-def convert_unit(type):
-    # Converts units based, grouped by type of measurement
-
-    print(f"\n: : {type} OPTIONS : :\n")  # prints single char, but should print word
-    # Variables defined by user input
-    if type == "M":
-        unit = input("(g) or (oz): ")
-        num = float(input("amount: "))
-
-        # Conditionally handles & displays data
-        if unit.lower() == "g":
-            converted = num / 28.35
-            print(f"\n{num} {unit} = {converted} oz")
-        else:
-            converted = num * 28.35
-            print(f"\n{num} {unit} = {converted} g")
+def select_unit(measurement):
+    # Conditionally handles prompt to correctly notate into 'calculate'
+    if measurement == "l":
+        prompt = generate_prompt("length")
+    elif measurement == "m":
+        prompt = generate_prompt("mass")
+    elif measurement == "v":
+        prompt = generate_prompt("volume")
     else:
-        print(f"\ninvalid type: {type}")
-    prompt(type)
+        # error handling
+        print(f"\ninvalid measurement: {measurement}")
+
+    selected_unit = input(prompt)
+    convert_unit(selected_unit.lower(), measurement)
 
 
-def prompt(measurement_unit):
-    # allows users to convert units again or exit the program
-    answer = input("\n(c)onvert again, s(witch) units, or (e)xit? ")
-    if answer.lower() == "c":
-        convert_unit(measurement_unit)
-    elif answer.lower() == "s":
-        select_measurement_type()
+def convert_unit(unit, type):
+    # Captures & type converts user data
+    num = float(input("enter amount: "))
+
+    # Conditionally calculates & displays data
+    if unit == "g":
+        converted = num / 28.35
+        print(f"\n{num} {unit} = {converted} oz")
+    elif unit == "oz":
+        converted = num * 28.35
+        print(f"\n{num} {unit} = {converted} g")
+    else:
+        print(f"\nformula not yet implemented for unit: {unit}")
+    display_menu(unit, type)
+
+
+def display_menu(unit, type):
+    # Displays prompt to dynamically capture user data
+    prompt = generate_prompt("menu")
+    selected_option = input(prompt)
+
+    # Conditionally handles user selection
+    if selected_option == "1":
+        convert_unit(unit, type)
+    if selected_option == "2":
+        select_unit(type)
+    elif selected_option == "3":
+        select_measurement()
     else:
         print(f"\n: : exiting {app} : : ")
         return
